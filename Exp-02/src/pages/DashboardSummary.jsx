b3c6a9473cd57cf63ed650fa06e374888d2939cc
Astik_Joshi <becomeyourself12@gmail.com>
2026-02-02 19:57:05 +0530

    Delete Exp-02/src directory

--- Exp-02/src/pages/DashboardSummary.jsx
+++
@@ -1,9 +0,0 @@
-const DashBoardSummary=()=>{
-    return(
-        <div>
-            <h3>Summary</h3>
-            <p>Summary content goes here.</p>
-        </div>
-    )
-}
-export default DashBoardSummary;
